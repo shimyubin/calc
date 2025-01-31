79040c1c3e42d403bf5213954620efae7549340d
shimyubin <tla1161@naver.com>
2025-01-31 16:47:16 +0900

    test1

--- index.js
+++ index.js
@@ -6,19 +6,11 @@ function subtract(a, b) {
   return a- b;
 }
 
-<<<<<<< HEAD
 function mul(a, b) {
   return a * b;
 }
 
-function div(a, b) {
-=======
-function multiply(a, b) {
-  return a * b;
-}
-
 function divide(a, b) {
->>>>>>> branch1
   return a / b;
 }
 
